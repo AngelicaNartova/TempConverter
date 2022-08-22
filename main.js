2bebeb2b1e2e318f2d29e94ef2a79ab026aaa783
Anzhelika Nartova <angelika26061997@gmail.com>
2022-08-22 15:03:30 -0400

    Background change depending on temperature

--- main.js
+++ main.js
@@ -9,35 +9,38 @@ for (let i = 0; i < inputs.length; i++) {
 
     input.addEventListener('input', function (e) {
         let value = parseFloat(e.target.value);
-        
+
         switch (e.target.name) {
 
             case "celcius":
-                
+
                 farhenInput.value = (value * 1.8) + 32;
                 kelvInput.value = value + 273.15;
-               
                 break;
 
             case "fahrenheit":
+
                 celInput.value = (value - 32) / 1.8;
                 kelvInput.value = ((value - 32) / 1.8) + 273.15;
                 break;
 
             case "kelvin":
+
                 celInput.value = value - 273.15;
                 farhenInput.value = ((value - 273.15) * 1.8) + 32;
-                break;     
+                break;
         }
 
+        let newBackground;
 
         if (celInput.value >= 20) {
-            
-            let backgroung = document.getElementById('main').classList.add('greenbackground');
-           
+            newBackground = document.getElementById('main').classList.add('greenBackground');
+            return newBackground;
 
         } else {
-            let coldBackgroung = document.getElementById('main').classList.add('coldBackground');
-        }
-    })
+            newBackground = document.getElementById('main').classList.add('coldBackground');
+            return newBackground;
+
+        } 
+    });
 };
