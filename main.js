b9c55c5e6869ba24cceca8fac1a942fe9e88c04a
AngelicaNartova <anzhelika26061997@gmail.com>
2022-09-25 14:45:51 -0400

    added classes and updated js

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ const farhenInput = document.getElementById('fahrenheit');
 const inputs = document.getElementsByClassName('input');
 const inputArray = [...inputs];
 
-let newClassBackgrnd = document.getElementById('main');
-
 inputArray.forEach((input, index, arr) => {
     input.addEventListener('input', (event) => {
 
@@ -15,7 +13,9 @@ inputArray.forEach((input, index, arr) => {
             farhenInput.value = (value * 1.8) + 32;
         } else {
             celInput.value = (value - 32) / 1.8;
-        }       
+        }  
+        
+        let newClassBackgrnd = document.getElementById('main');     
 
         if (celInput.value >= 25) {
             newClassBackgrnd.className = "hotBackground";
